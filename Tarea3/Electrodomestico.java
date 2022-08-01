1dd7cd7debf550c5f5bc12f7af2c49858014d2ea
kathe <Kathe07338@gmail.com>
2022-08-01 22:18:11 +0400

    Crear una clase de electrdomesticos.

---
+++ Tarea3/Electrodomestico.java
@@ -0,0 +1,19 @@
+package Tarea3;
+
+public class Electrodomestico {
+
+    Integer precioBase;
+    String color;
+    String consumoEnergetico;
+    Integer peso;
+
+
+    // Constructor
+    Electrodomestico (int i, String string, String string2, int jInteger precioBase; String color; String consumoEnergetico;Integer peso) {
+        this.precioBase = PrecioBase;
+        this.color= Color;
+        this.consumoEnergetico = ConsumoEnergetico;
+        this.peso=Peso;
+
+    }
+}
---
+++ Tarea3/Main.java
@@ -0,0 +1,12 @@
+package Tarea3;
+
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Electrodomestico Electrodomestico = new Electrodomestico (100, "blanco", "F", 5);
+    }
+}
+
+//Integer precioBase; String color; Integer consumoEnergetico;Integer peso;
